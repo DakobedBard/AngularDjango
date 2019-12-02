649122f5297528a16541a1488599c45b6d9c858d
JasonHeyward <mddarr@gmail.com>
2019-12-01 20:51:37 -0800

    Cleaned up the code that wasn't working.

--- app/accounts/api/urls.py
+++ app/accounts/api/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import url, include
 from django.urls import path, include, re_path
-from accounts.api.views import UserLoginView
+from accounts.api.views import UserLoginView, UserCreateAPIView
 
-from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token # accounts app
-
-from .views import AuthAPIView, RegisterAPIView, UserCreateAPIView
 urlpatterns = [
-    re_path(r'^$', AuthAPIView.as_view(), name='loginView'),
     re_path(r'^register/$', UserCreateAPIView.as_view(), name='register'),
     re_path(r'login/', UserLoginView.as_view(),name='login')
 ]
