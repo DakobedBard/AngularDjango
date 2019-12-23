bdb335f8f68958fa23f65b2436b29d025f0d41d5
JasonHeyward <mddarr@gmail.com>
2019-12-23 15:19:29 -0800

    I am adding the login and register components... they are almost ready to make request to the API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -30,6 +30,7 @@ import { RegisterComponent } from './register/register.component'
     HttpClientModule,
     BrowserAnimationsModule,  
     MaterialModule,
+    ReactiveFormsModule,
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
