ee0c20a1308bdee60042cc2488e0240740a2ac52
JasonHeyward <mddarr@gmail.com>
2019-12-22 21:09:23 -0800

    Try to upload files, will merge then keep on testing this API route, i was getting a 500..

--- app/base/settings.py
+++ app/base/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "tabs",
     "accounts",
+    "uploadapp"
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- app/base/urls.py
+++ app/base/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import LoginView
 from rest_framework.authtoken import views
 from accounts.views import login, signup
 import tabs.views
 
-
+from uploadapp.views import FileUploadView
 urlpatterns = [
     path("admin/", admin.site.urls),
     re_path('home/', tabs.views.home, name ='home'),
@@ -18,6 +16,10 @@ urlpatterns = [
     # Users API
     re_path(r'^api/auth/', include('accounts.api.urls'), name='api-auth'),
     path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
+
+
+    # Upload
+    path('upload/', include('uploadapp.urls')),
 
 ]
 
