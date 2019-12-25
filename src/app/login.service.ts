b0351270ca01815e02aaef59242453092931ad1a
JasonHeyward <mddarr@gmail.com>
2019-12-25 02:29:57 -0800

    I am hitting the login endpoint from the UI though need to validate..

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { User } from './user'
+const headers = new HttpHeaders({
+  'Content-Type': 'application/json'
+});
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor( httpClient: HttpClient ) { }
-  loginUser
+  apiURL: string = 'http://localhost:8000/api/auth';
+  constructor(private  httpClient:HttpClient) { }
+  public loginUser(user: User){
+    return this.httpClient.post(`${this.apiURL}/login/`,user, {headers:headers});
+}
 }
