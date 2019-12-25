47c5173451e099442fe6d5eefa27b0a20a136328
JasonHeyward <mddarr@gmail.com>
2019-12-25 02:21:23 -0800

    start to try to implement login...

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,ReactiveFormsModule, FormBuilder,FormArray, Validators, FormControl } from '@angular/forms';
 import { LoginService } from '../login.service';
+import {User} from '../user'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private loginService: LoginService) { }
+  loginForm: FormGroup;
+  isSubmitted  =  false;
+  dataSaved = false;
+  constructor(private loginService: LoginService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm  =  this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login(){
+    this.dataSaved = false;
+    this.isSubmitted= true;
+    let user = this.loginForm.value
+    this.loginUser(user);
+  }
+  
+  loginUser(user:User){
+    this.loginService.loginUser(user).subscribe(
+      user => { console.log(user);this.dataSaved = true;
+      },
+      err => {
+        console.log(err);
+      }
+    )
   }
 
 }
+
+
+
+
+  
+
+
