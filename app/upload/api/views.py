e945b21e2cac2f3e05005ef4b0fbac58a8f925bd
JasonHeyward <mddarr@gmail.com>
2019-12-25 15:42:43 -0800

    Trying to pass the ID to the documents API ..

--- app/upload/api/views.py
+++ app/upload/api/views.py
@@ -10,12 +10,23 @@ class DocumentListAPIView(mixins.CreateModelMixin, generics.ListAPIView):
     def get_queryset(self):
         qs = Document.objects.all()
         query = self.request.GET.get("q")
+        userID = self.request.query_params.get('id', None)
+
         if query is not None:
             qs = qs.filter(Q(title__icontains=query))
         return qs
     def post(self,request,*args, **kwargs):
         return self.create(request, *args, **kwargs)
+class DocumentAPIView(mixins.CreateModelMixin,generics.ListAPIView):
+    serializer_class = DocumentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
+    def get_queryset(self):
+        qs = Document.objects.all()
+        query = self.request.GET.get("q")
+        if query is not None:
+            qs = qs.filter(Q(title__icontains=query))
+            return qs
 
 class DocumentDetailAPIView(generics.RetrieveAPIView):
     queryset = Document.objects.all()
