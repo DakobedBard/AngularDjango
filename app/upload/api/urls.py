74396184d6b9a80398fb8b11d62ef876d2f4d112
JasonHeyward <mddarr@gmail.com>
2019-12-25 16:02:13 -0800

    I am passing an ID through the path..

--- app/upload/api/urls.py
+++ app/upload/api/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     re_path(r'^$', DocumentListAPIView.as_view(), name='list'),
     re_path(r'^(?P<pk>\d+)/$', DocumentDetailAPIView.as_view(), name='detail'),
     re_path(r'create/$', DocumentCreateAPIView.as_view(), name='create'),
-    re_path(r'list/', DocumentAPIView.as_view(), name='list-users')
+    re_path(r'^list/(?P<id>.+)/$', DocumentAPIView.as_view(), name='list-users')
 ]
