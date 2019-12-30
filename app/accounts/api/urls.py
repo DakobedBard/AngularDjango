2e470fd5304d1a9017d515c7ac077d15c71e96fc
JasonHeyward <mddarr@gmail.com>
2019-12-30 01:52:16 -0800

    Plug Away, it seems to work at the time being..

--- app/accounts/api/urls.py
+++ app/accounts/api/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     re_path(r'register/$', UserCreateAPIView.as_view(), name='register'),
     re_path(r'login/', UserLoginView.as_view(),name='login'),
     re_path(r'api/auth/token',obtain_jwt_token),
-    re_path('rest-auth/', include('rest_auth.urls')),
-
-
 ]
