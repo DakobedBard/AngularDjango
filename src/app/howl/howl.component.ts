852023ca110f030a3d119c61e2092b15794d5f0c
JasonHeyward <mddarr@gmail.com>
2020-01-01 15:31:39 -0800

    temp

--- src/app/howl/howl.component.ts
+++ src/app/howl/howl.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HowlService } from '../howl.service'
 @Component({
   selector: 'app-howl',
   templateUrl: './howl.component.html',
   styleUrls: ['./howl.component.css']
 })
 export class HowlComponent implements OnInit {
+  index = 0;
+  // elms = ['track', 'timer', 'duration', 'playBtn', 'pauseBtn','prevBtn', 'nextBtn',
+  //  'playlistBtn', 'volumeBtn', 'progress', 'bar','wave', 'loading', 'playlist', 
+  //  'list', 'volume', 'barEmpty', 'barFull', 'sliderBtn'];
+  //  window: any;
 
-  constructor() { }
-
+  constructor(howelService: HowlService){
+    // this.elms.forEach(function(elm) {
+    //   this.window[elm] = document.getElementById(elm);
+    // });
+  }
+  
+  playlist(songArray: any) {
+       return songArray.length;
+  }
   ngOnInit() {
   }
 
--- src/app/player.ts
+++ src/app/player.ts
@@ -1,1 +1,20 @@
 import {Howl, Howler} from 'howler';
+
+export class Player {
+  index = 0;
+  elms = ['track', 'timer', 'duration', 'playBtn', 'pauseBtn','prevBtn', 'nextBtn',
+   'playlistBtn', 'volumeBtn', 'progress', 'bar','wave', 'loading', 'playlist', 
+   'list', 'volume', 'barEmpty', 'barFull', 'sliderBtn'];
+   window: any;
+
+  constructor(){
+
+    this.elms.forEach(function(elm) {
+      this.window[elm] = document.getElementById(elm);
+    });
+    
+  }
+  playlist(songArray: any) {
+      return songArray.length;
+  }
+}
