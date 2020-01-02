8fe35816495510e2c1c38fe21390d390b495a856
JasonHeyward <mddarr@gmail.com>
2020-01-02 01:12:18 -0800

    Making baby steps towards getting this module to work..

--- src/app/tabs/tab.ts
+++ src/app/tabs/tab.ts
@@ -1,31 +1,53 @@
 export class TabLine{
-    measures: Array<Measure>;
+    measures: Measure[] = []
+    outputString;
     constructor(){
+
+        let measure = new Measure([{
+                gString:"e",
+                fret:2,
+                beat:2,
+        }])
+        this.measures.push(measure)
+    }
+    generateString(){
+        this.measures.forEach(measure => {
+            this.outputString += measure.measureString;
+        });
+        return this.outputString;
     }
 }
 
 export class Measure{
     notes;
-    constructor(notes){
+    outputString;
+    constructor(notes: Array<Note>){
         this.notes = notes;
-    }
-    generateString(){
+        let beatIndex = 0;
+        let beats = []
+        notes.forEach(note => {
+            this.addtoSring(note)
+            beats.push(note.beat)    
+        });
 
     }
+    addtoSring(note: Note){
+        this.outputString += "$2 5 3 0 "
+    }
+    generateString(){
+    }
     measureString(){
-        // Return a string representation of the measure..
+        return this.outputString + '||'
     }
 }
 
 export class Note{
     fret;
     gString;
-    noteLength;
     beat;
-    constructor(fret, gString, noteLength, beat){
+    constructor(fret, gString, beat){
         this.gString = gString;
         this.fret = fret;
-        this.noteLength = noteLength
         this.beat = beat
     }
 }
--- src/app/tabs/tab/tab.component.ts
+++ src/app/tabs/tab/tab.component.ts
@@ -7,7 +7,8 @@ import { TabLine }from '../tab'
 })
 export class TabComponent implements OnInit {
   constructor() { 
-
+    this.tablines.push(new TabLine());
+    
   }
   tab;
   chords
@@ -16,16 +17,15 @@ export class TabComponent implements OnInit {
   totalMeasures;
   notevalue = 4
   beatsPerBar = 4;
-  lines: Array<TabLine>;
+  tablines: TabLine[] = []
   ngOnInit() {
+
     this.tab  = [
       {tab_string: '$4.7/9.$3.6/8.$2.5/7 9p7 $2.9.$3.9.$4.9 ||'},
-      {tab_string: '$A 0 1 3 | $2 5 3 0 ||'}
+      {tab_string: '$A  1 3 | $2 5 3 0 ||'},
+      {tab_string: this.tablines.pop().generateString()}
     ];
-    this.chords = [
-      {tab_string: 'G / / / | Bm / C / ||'},
-      {tab_string: 'D / / / | Am / B / ||'},
-    ]
+
   };
 
 
