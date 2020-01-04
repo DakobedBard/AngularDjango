d30533e0d24be65265f471e4efcf737067d35d29
JasonHeyward <mddarr@gmail.com>
2020-01-03 17:07:11 -0800

    Add tab-list component

---
+++ src/app/tabs/tab-list/tab-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { TabService } from '../tab.service'
+@Component({
+  selector: 'app-tab-list',
+  templateUrl: './tab-list.component.html',
+  styleUrls: ['./tab-list.component.css']
+})
+export class TabListComponent implements OnInit {
+  tabService: TabService
+  tabs;
+  constructor(tabService: TabService) { 
+    this.tabService = tabService;
+  }
+
+  getTabs(){
+    this.tabService.getTabs().subscribe(
+      (data) => {
+      console.log("The data is ! " + data)
+      for (const tab of (data as any)) {
+        this.tabs.push({
+          notes: tab.notes,
+          name:tab.name
+        });
+      }
+      console.log("why")
+      this.tabs.forEach(tab => {
+        console.log("first tab is " + tab.name)
+      });
+      },
+      (err) => {  
+        console.log(err);
+      }
+    );
+  }
+  
+  ngOnInit() {
+    this.getTabs()
+  }
+
+}
